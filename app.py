0c95bd09519ea2aa2355a8c37c7fa99a85a7e24f
Pawel Fijalkowski <pawel.fijalkowski@pl.ibm.com>
2019-12-09 11:01:38 +0100

    moving to posgresql

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #pip install pylint-flask
+import os
 
 from flask import Flask
 from flask_restful import Api
@@ -10,7 +11,13 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__) # pylint: disable=invalid-name
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+
+'''
+postgres://ylhxutjippoikm:c1035be17049d7a553c641682bf7f159bf622071977a064bae2dbfe950ad3edf@ec2-54-228-246-214.eu-west-1.compute.amazonaws.com:5432/d8t43ttlge1b3a
+'''
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
